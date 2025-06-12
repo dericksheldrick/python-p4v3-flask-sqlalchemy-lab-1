309469b2c2108688e83f357409a81f16c210c25a
Derick Sheldrick <sheldrickderick9@gmail.com>
2025-06-12 07:20:36 +0300

    flask-sqlalchemy lab1

--- server/app.py
+++ server/app.py
@@ -21,7 +21,40 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake = db.session.get(Earthquake, id)
+    if earthquake:
+        data ={
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year" : earthquake.year
+        }
+        response = make_response(data, 200)
+    else:
+        error_message = {"message":f'Earthquake {id} not found.'}
+        response = make_response(error_message, 404)
+    
+    return response
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    list = [{
+        "id":quake.id,
+        "location": quake.location,
+        "magnitude": quake.magnitude,
+        "year": quake.year
+
+    } for quake in earthquakes]
+
+    response = {
+        "count":len(list),
+        "quakes": list
+    }
+
+    return make_response(response, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
